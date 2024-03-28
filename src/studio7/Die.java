311e38b6ffe43785c9ec420dc6b074c3a51143bf
nlevy <nlevy@NoahL_Surface.dhcp.wustl.edu>
2024-03-28 12:05:32 -0500

    Finished

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,5 +1,21 @@
 package studio7;
 
 public class Die {
-
+	private int sides;
+	
+	
+	public Die(int sides) {
+		this.sides=sides;
+	}
+	public int GetSides() {
+		return sides;
+	}
+	
+	public void SetSides(int newSides) {
+		this.sides=newSides;
+	}
+	
+	public int rollDie() {
+		return (int)(Math.random()*sides)+1;
+	}
 }
